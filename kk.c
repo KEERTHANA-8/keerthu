93ba03ca7c6b542545372414182c0316c996d332
KEERTHANA-8 <keerthana.s1413047@ksrce.ac.in>
2017-03-13 17:12:08 +0530

    Add files via upload

---
+++ kk.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+  int i,n;
+  printf("\n enter the number");
+  scanf("%d",&n);
+  for(i=0;i<n;i++)
+  {
+   printf("\n HELLO");
+   }
+   getch();
+   }5
